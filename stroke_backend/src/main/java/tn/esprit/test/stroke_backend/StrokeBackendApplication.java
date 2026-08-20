8564c3d8f190c6183759f7760423923ec15fe774
hazem324 <hazemhadda2002@gmail.com>
2026-08-20 23:33:53 +0100

    backend setup

---
+++ stroke_backend/src/main/java/tn/esprit/test/stroke_backend/StrokeBackendApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.test.stroke_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrokeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrokeBackendApplication.class, args);
+    }
+
+}
---
+++ stroke_backend/src/test/java/tn/esprit/test/stroke_backend/StrokeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.test.stroke_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrokeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
