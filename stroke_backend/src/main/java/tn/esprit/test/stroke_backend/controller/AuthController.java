b2f9d2ed38eb68672eff04915b68104f151fe0e3
hazem324 <hazemhadda2002@gmail.com>
2026-08-21 22:23:29 +0100

    create api signup

--- stroke_backend/src/main/java/tn/esprit/test/stroke_backend/controller/AuthController.java
+++ stroke_backend/src/main/java/tn/esprit/test/stroke_backend/controller/AuthController.java
@@ -1,18 +1,18 @@
 package tn.esprit.test.stroke_backend.controller;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import jakarta.validation.Valid;
+
+import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import lombok.RequiredArgsConstructor;
-
 import tn.esprit.test.stroke_backend.dto.auth.RegisterRequest;
 import tn.esprit.test.stroke_backend.entities.User;
+import tn.esprit.test.stroke_backend.exceptions.EmailAlreadyExistsException;
 import tn.esprit.test.stroke_backend.services.servicesInterface.IAuthService;
 
 @RestController
@@ -26,16 +26,33 @@ public class AuthController {
     public ResponseEntity<Map<String, Object>> register(
             @Valid @RequestBody RegisterRequest request) {
 
-        User user = authService.register(request);
+        try {
 
-        Map<String, Object> response = new HashMap<>();
+            User user = authService.register(request);
 
-        response.put("message", "Compte créé avec succès");
-        response.put("userId", user.getId());
-        response.put("email", user.getEmail());
+            return ResponseEntity
+                    .status(HttpStatus.ACCEPTED)
+                    .body(Map.of(
+                            "message", "Compte créé avec succès",
+                            "userId", user.getId(),
+                            "email", user.getEmail()
+                    ));
 
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(response);
+        } catch (EmailAlreadyExistsException e) {
+
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(Map.of(
+                            "message", "Cette adresse e-mail est déjà utilisée"
+                    ));
+
+        } catch (Exception e) {
+
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of(
+                            "message", "Une erreur interne est survenue sur le serveur"
+                    ));
+        }
     }
 }
